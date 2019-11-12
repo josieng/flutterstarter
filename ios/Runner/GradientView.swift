6915df9b2f79b5cdf528cda60270289bebd49874
Damon Yuan <damon.yuan.dev@gmail.com>
2019-11-12 16:26:29 +0800

    add iOS splash View

---
+++ ios/Runner/GradientView.swift
@@ -0,0 +1,34 @@
+//
+//  GradientView.swift
+//  Runner
+//
+//  Created by Damon Yuan on 12/11/2019.
+//  Copyright © 2019 The Chromium Authors. All rights reserved.
+//
+
+import Foundation
+
+@IBDesignable
+class GradientView: UIView {
+    @IBInspectable var firstColor: UIColor = UIColor.clear {
+        didSet {
+            updateView()
+        }
+    }
+    @IBInspectable var secondColor: UIColor = UIColor.clear {
+        didSet {
+            updateView()
+        }
+    }
+    
+    override class var layerClass: AnyClass {
+        get {
+            return CAGradientLayer.self
+        }
+    }
+    
+    func updateView() {
+        let layer = self.layer as! CAGradientLayer
+        layer.colors = [firstColor, secondColor].map{$0.cgColor}
+    }
+}
